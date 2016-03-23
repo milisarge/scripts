3ac4887c2c5c160303b7e1d9a745d7051b7af43e
root <root@lokal>
2016-03-23 18:27:07 +0200

    yeni

---
+++ ubu_ilkurpak.sh
@@ -0,0 +1,4 @@
+apt-get install bsdtar binutils gcc g++ m4 make bison gawk texinfo pkg-config squashfs-tools lzip
+mv /usr/bin/mawk /usr/bin/mawk-eski
+apt-get install git geany samba rdesktop hexchat qemu nmap cups-bsd  syslinux-utils
+
---
+++ uzak_sshfs_bagla.sh
@@ -0,0 +1,3 @@
+ip=1.1.1.1
+umount /opt/baglamadizini
+sshfs root@$ip:/opt/ /opt/baglamadizini
