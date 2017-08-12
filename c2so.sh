9bbbf304900b117799b0f64ec26052c1718817d8
milisbir <milisarge@gmail.com>
2017-08-12 13:35:30 +0300

    c2so-py2bin

--- c2so.sh
+++ c2so.sh
@@ -1,1 +1,5 @@
 gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/usr/include/python3.5m -o yourmod.so yourmod.c
+#py to binary
+cython --embed -o dhtbil.c dhtbil.py
+gcc `python3-config --cflags --ldflags` dhtbil.c -o dhtbil
+gcc -I /usr/include/python3.5m/ -L /usr/lib/python3.5 -lpython3.5m dhtbil.c -o dhtbil
